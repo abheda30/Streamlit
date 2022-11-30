4311542293bb21f52a1615d7d64e4803085b8e40
sravipa2 <sravipa2@uncc.edu>
2022-11-30 21:27:58 +0000

    Code Changes

--- pages/page2.py
+++ pages/page2.py
@@ -1,8 +1,8 @@
-import plotly.express as px
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 # loading Dataset 
 df = pd.read_csv('ChurnDataset.csv')
