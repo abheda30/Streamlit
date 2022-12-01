bfa5385775f16ce0c1f0dead642426288ec77a3c
sravipa2 <sravipa2@uncc.edu>
2022-12-01 17:39:19 +0000

    Code Chanes

--- pages/Analysis_of_Telecommunication_Data.py
+++ pages/Analysis_of_Telecommunication_Data.py
@@ -8,19 +8,17 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('ChurnDataset.csv')
 st.title("Analysis of Telecommunication Data")
 
-list = ['Dataset','Prevailing Customers Statistics','Total Customers Statistics', 'Gender Statistics']
+list = ['Dataset','Prevailing Customers Statistics','Left Customers Statistics','Total Customers Statistics', 'Gender Statistics']
 
 pageview = st.sidebar.radio('Select the page you want to view', list)
 
 if pageview == 'Dataset':
     st.write(df)
     
-df['Churn'].replace(['Yes', 'No'],
-                        [0, 1], inplace=True)
-
-    
 if pageview == 'Prevailing Customers Statistics':
     st.subheader("Churn Value = No.of Customers within the Company ")
+    df['Churn'].replace(['Yes', 'No'],
+                        [0, 1], inplace=True)
     f1 = st.radio("Select one among these:", ['gender','InternetService', 'Contract'])
     f2 = st.selectbox("Selected Column:", ['Churn'])
     
@@ -87,4 +85,32 @@ if pageview == 'Gender Statistics':
         dfe = dfe.sort_values(by=[f1],ascending=False)
         dfe = dfe.reset_index()
         fige = px.bar(dfe, x=f1, y=f2, color=f1, title="Bar chart of "+f1+" and "+f2)
+        st.plotly_chart(fige)
+
+if pageview == 'Left Customers Statistics':
+    st.subheader("Churn Value = No.of Customers left the Company ")
+    df['Churn'].replace(['No', 'Yes'],
+                        [0, 1], inplace=True)
+    f1 = st.radio("Select one among these:", ['gender','InternetService', 'Contract'])
+    f2 = st.selectbox("Selected Column:", ['Churn'])
+    
+    if f1 == 'gender':
+        dfe = df.groupby([f1]).sum()
+        dfe = dfe.sort_values(by=[f1],ascending=False)
+        dfe = dfe.reset_index()
+        fige = px.bar(dfe, x=f1, y=f2, color=f1, title="Bar chart of "+f1+" and "+f2)
+        st.plotly_chart(fige)
+    
+    if f1 == 'InternetService':
+        dfe = df.groupby([f1]).sum()
+        dfe=dfe.sort_values(by=[f1],ascending=False)
+        dfe = dfe.reset_index()
+        fige = px.bar(dfe, x=f1, y=f2, color=f1, title="Bar chart of "+f1+" and "+f2)
+        st.plotly_chart(fige)
+     
+    if f1 == 'Contract':
+        dfe = df.groupby([f1]).sum()
+        dfe = dfe.sort_values(by=[f1],ascending=False)
+        dfe = dfe.reset_index()
+        fige = px.bar(dfe, x=f1, y=f2, color=f1,title="Bar chart of "+f1+" and "+f2)
         st.plotly_chart(fige)
